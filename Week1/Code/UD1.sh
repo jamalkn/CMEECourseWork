08ca02655b5207a6458a737f57dc19f1e1b5c291
Jamal Khan <jamal.khan@btinternet.com>
2018-10-10 04:43:29 +0100

    M2

--- Week1/Code/UD1.sh
+++
@@ -1,6 +0,0 @@
-echo "Remove    excess      spaces." | tr -s "\b" " "
-echo "remove all the as" | tr -d "a"
-echo "set to uppercase" | tr [:lower:] [:upper:]
-echo "10.00 only numbers 1.33" | tr -d [:alpha:] | tr -s " " ","
-
-
